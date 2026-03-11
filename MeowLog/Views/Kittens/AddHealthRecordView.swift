0b1dd5a89a510209fa76c598ee2569d84fccfa93
zhaoshian <295057329@qq.com>
2026-03-11 17:33:14 +0800

    fix: 补充缺失的 SwiftData import（HealthRecordListView, AddHealthRecordView, KittenDetailView）

--- MeowLog/Views/Kittens/AddHealthRecordView.swift
+++ MeowLog/Views/Kittens/AddHealthRecordView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftData
 
 struct AddHealthRecordView: View {
     @Environment(\.modelContext) private var modelContext
--- MeowLog/Views/Kittens/KittenDetailView.swift
+++ MeowLog/Views/Kittens/KittenDetailView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftData
 
 struct KittenDetailView: View {
     @Environment(\.modelContext) private var modelContext
